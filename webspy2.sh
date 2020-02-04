db901f38f8b74230152bb2c7f04bbfbd6aa3a4f2
Jeremy <wacrochester@gmail.com>
2020-02-04 03:46:14 -0800

    Update webspy2.sh

--- webspy2.sh
+++ webspy2.sh
@@ -1,2 +1,5 @@
-echo sends URLs sniffed from a client to your local browser;
-webspy -i wlan0
+#!/bin/bash
+echo "sends URLs sniffed from a client to your local browser"
+echo -n "Interface: "
+read iface
+sudo webspy -i $iface
